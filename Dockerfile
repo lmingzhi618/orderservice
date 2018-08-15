3323c7ce1223e5b91329042a5f5f77456a19b851
mingzhi198 <mingzhi198@qq.com>
2018-08-15 10:54:22 +0800

    docker swarm deploy

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,6 @@
-FROM golang:latest
-
-RUN go get github.com/6xiao/go/Common
-RUN go get github.com/bitly/go-simplejson
-RUN go get github.com/go-sql-driver/mysql
-
-ADD . $GOPATH/src/orderservice
-WORKDIR $GOPATH/src/orderservice
-RUN go build .
+FROM scratch 
+WORKDIR /app
+COPY . /app
 
 EXPOSE 8080
-
 ENTRYPOINT ["./orderservice"]
--- start.sh
+++ start.sh
@@ -1,20 +1,14 @@
 #! /usr/bin/sh
 
-# 1. get mysql
-#docker pull mysql
-
-# 2. build mysql image
+# add read and write privilege for all users(mysql)
+mkdir db_data && chmod 777 db_data
+# build mysql image
 docker build -t order_mysql ./db/
 
-# 3. run mysql container
-#docker run --rm -p 3306:3306 -e MYSQL_ROOT_PASSWORD=abc123456 --name order_mysql -d order_mysql
-docker run --rm -p 3306:3306 -e MYSQL_ROOT_PASSWORD=abc123456 --name db -d order_mysql
+# build app service, the image has been pushed to docker 
+#docker build -t orderservice .
 
-# 4. build app service
-docker build -t orderservice .
-
-# 5. run app service and link mysql db
-#docker run --rm -p 8080:8080 --link order_mysql:mysql --name orderservice -d orderservice 
-docker run --rm -p 8080:8080 --link db:mysql --name orderservice -d orderservice 
+# get image from docker and deploy
+docker stack deploy -c docker-compose.yml order_svr
 
 
